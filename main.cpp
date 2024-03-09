ef97766296880f15f4879e3a74839b92512c0d55
TealSmurf <jcoin707@gmail.com>
2024-03-08 23:49:13 -0800

    Finished created basic parameters, now working on main part that displays majority of values.

--- main.cpp
+++ main.cpp
@@ -9,17 +9,24 @@ int main()
    cin >> N >> M;
    if(0 < N < 10 && 1 <= M <= 10)
    {
-   for(int i = 0; i < M; i++)
-   {
-      if(N = 0)
+      for(int i = 0; i < M; i++)
       {
-         cout << 0;
+         if(N = 0)
+         {
+            cout << 0;
+         }  
+         else if(i = 0)
+         {
+            cout << 1;
+         }
+         else
+         {
+            for(int i = 0; i < M-1; i++)
+            {
+               
+            }
+         }
       }
-      else if(i = 0)
-      {
-         cout << 1;
-      }
-      else 
    }
   
 }
